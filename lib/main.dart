7ef3971b634a1e85fbc9473b992434d64a973860
Your Name <you@example.com>
2024-09-11 13:33:30 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:auth_task/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
